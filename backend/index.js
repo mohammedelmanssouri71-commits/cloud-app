e1fd4c7964fb756da5ad40ef7caa7791a527526f
EL-MANSSOURI MOHAMMED <MOHAMMED.EL-MANSSOURI@CMCTTA.MA>
2026-04-06 13:05:16 +0100

    modifier dockerfile

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mysql = require('mysql2');
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
